5625c62b43d273ca5677bf7ea34df260a501152d
marmous <yoann.calamai@marmous.net>
2024-08-14 22:45:10 +0200

    release script

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+d=$(date +%Y.%-m.%-d)
+version="$d.1"
+
+cd dist
+mkdir temp
+cd temp
+
+echo "Creating internal extension chrome package"
+cp -r ../../src/* .
+mv -f manifest-internal.json manifest.json
+zip -r ../release-internal-$version.zip .
+
+echo "Creating public extension chrome package"
+cp -f ../../src/manifest-internal.json manifest.json
+rm readme.txt
+zip -r ../release-$version.zip .
+
+cd ..
+rm -rf temp
